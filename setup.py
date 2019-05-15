83019240edc419c825fe4617fe87063175199621
Francesco Battaglia <fpbattaglia@gmail.com>
2019-05-15 16:24:26 +0200

    adding scripts to setup.py

--- setup.py
+++ setup.py
@@ -8,5 +8,6 @@ setup(
     license='GPLv3',
     author='Francesco Battaglia ',
     author_email='fpbattaglia@gmail.com',
-    description='"old" spike sorting pipeline, used e.g. for Arbab et al (2018), Cabral et al. (2014)'
+    description='"old" spike sorting pipeline, used e.g. for Arbab et al (2018), Cabral et al. (2014)',
+    scripts=['spikePreprocess/filter_compute_features.py', 'spikePreprocess/filter_compute_peak_time.py']
 )
